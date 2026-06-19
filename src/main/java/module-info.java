e21898df391833f136d1bd3c6f20120edfc60fd3
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-06-19 15:55:39 +0700

    chore: add json library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,8 +7,9 @@ module osk {
     
     requires org.apache.pdfbox;
     requires org.apache.pdfbox.io;
-
+    requires org.json;
 
     opens osk to javafx.fxml;
+    opens osk.app to javafx.fxml;
     exports osk;
 }
