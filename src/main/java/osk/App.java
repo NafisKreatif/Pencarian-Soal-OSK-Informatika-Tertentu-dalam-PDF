7b4383531cc022c643f72b463b26db9aae631bf0
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-06-19 20:40:24 +0700

    test: refresh scene with f5

--- src/main/java/osk/App.java
+++ src/main/java/osk/App.java
@@ -1,26 +1,13 @@
 package osk;
 
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.rendering.PDFRenderer;
 
 /**
  * JavaFX App
@@ -31,31 +18,24 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Main"), 640, 480);
-
-        // FileChooser fileChooser = new FileChooser();
-        // fileChooser.setTitle("Load PDF");
-        // fileChooser.getExtensionFilters().addAll(
-        //         new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
-
-        // File selectedFile = fileChooser.showOpenDialog(scene.getWindow());
-
-        // try (PDDocument document = Loader.loadPDF(new RandomAccessReadBufferedFile(selectedFile))) {
-        //     System.out.println("PDF opened successfully!");
-        //     System.out.println("Total Pages: " + document.getNumberOfPages());
-        //     PDFRenderer renderer = new PDFRenderer(document);
-        //     BufferedImage bufferedImage = renderer.renderImage(0);
-        //     WritableImage writableImage = SwingFXUtils.toFXImage(bufferedImage, null);
-
-        //     ImageView imageView = new ImageView(writableImage);
-        //     ScrollPane scrollPane = new ScrollPane(imageView);
-        //     root.getChildren().add(scrollPane);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+        scene = new Scene(loadFXML("Main"), 1080, 720);
 
         stage.setScene(scene);
         stage.show();
+
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.F5) {
+                try {
+                    scene = new Scene(loadFXML("Main"), 1080, 720);
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+                stage.setScene(scene);
+                stage.setMaximized(true);
+            }
+        });
+
+        stage.setMaximized(true);
     }
 
     static void setRoot(String fxml) throws IOException {
